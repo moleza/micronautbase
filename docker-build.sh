7aa3e6ef494b7d61471718a1f5462c19ecb19ef0
Sheldon du Trevou <sheldon@gomelo.co.za>
2022-05-11 12:34:54 +0200

    Updated Docker Config

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-docker build . -t micronautbase
+./gradlew dockerBuildNative
 echo
 echo
 echo "To run the docker container execute:"
 echo "    $ docker run -p 8080:8080 micronautbase"
+echo "Or to Run the compose file use:"
+echo "    $ docker-compose up"
